51be17b0d2b579d2750c0824b8de8a6f54e1c09f
Akshat Tiwari <akshat35tiwari@gmail.com>
2024-09-14 23:51:59 +0530

    Update A_Cover_in_Water.cpp

--- A_Cover_in_Water.cpp
+++ A_Cover_in_Water.cpp
@@ -1,18 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Rev( vector<int> &num, int k){
-    int n = num.size();
-    
-
-    return true;
-}
-
-int main(){
+int main()
+{
     int t;
     cin >> t;
-    while(t--){
-        
+    while (t--) {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (str[i] == '.') {
+                int sax = 0;
+                while(i < n && str[i] == '.') {
+                    sax++;
+                    i++;
+                }
+                if (sax >= 3) {
+                    count = 2;
+                    break;
+                } 
+                else {
+                    count += sax;
+                }
+                i--;
+            }
+        }
+        cout << count << endl;
     }
-        return 0;
+    return 0;
 }
