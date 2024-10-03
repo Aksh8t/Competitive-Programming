1628949f97e4d74a16132e83941c1181e9574adc
Your Name <akshat35tiwari@gmail.com>
2024-10-03 12:52:18 +0530

    New v1

--- A_Desorting.cpp
+++ A_Desorting.cpp
@@ -10,15 +10,42 @@ using ll = long long;
 using vi = vector<int>;
 using pii = pair<int, int>;
 
-
-int main() {
+int main()
+{
 
     int t;
     cin >> t;
 
-    while (t--) {
+    while (t--)
+    {
         // Your logic here
-        
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        int low = INT_MAX;
+        bool sortcond = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> nums[i];
+            if (i > 0)
+            {
+                low = min(low, nums[i] - nums[i - 1]);
+                if (nums[i] < nums[i - 1])
+                {
+                    sortcond = false;
+                }
+            }
+        }
+
+        if (!sortcond)
+        {
+            cout << 0 << nl;
+        }
+        else
+        {
+            cout << (low / 2) + 1 << nl;
+        }
     }
 
     return 0;
