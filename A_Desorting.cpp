238a0979fa1419cb011cfb31a3354e5a3387456b
Your Name <akshat35tiwari@gmail.com>
2024-10-03 12:37:30 +0530

    ok

---
+++ A_Desorting.cpp
@@ -0,0 +1,25 @@
+#define nl endl
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+#define neg1 cout << -1 << nl
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Your logic here
+        
+    }
+
+    return 0;
+}
